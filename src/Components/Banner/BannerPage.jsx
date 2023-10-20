51000e4d0da6d68ae8bb5514e95ac6efbda0b55e
asifbuet047 <asifbuet047@gmail.com>
2023-10-21 03:38:21 +0600

    Fix some BannerPage responsiveness bugs

--- src/Components/Banner/BannerPage.jsx
+++ src/Components/Banner/BannerPage.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function BannerPage({ banner }) {
     return (
-        <div className="hero h-1/6 bg-base-200 flex flex-col lg:flex-row lg:justify-around lg:pr-10 lg:pt-5">
+        <div className="hero h-1/6 bg-base-200 lg:justify-around lg:pr-10 lg:pt-5">
             <div className="hero-content flex-col lg:flex-row">
-                <img src={banner.product_banner_image} className="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 className="text-5xl font-bold">{banner.product_name}</h1>
-                    <p className="py-6">{banner.product_short_des}</p>
-                    <button className="btn btn-primary">Buy only ${banner.product_price}</button>
+                <img src={banner.product_banner_image} className="max-w-lg rounded-lg shadow-2xl" />
+                <div className='flex flex-col items-center lg:items-start'>
+                    <h1 className="text-2xl lg:text-5xl font-bold">{banner.product_name}</h1>
+                    <p className="text-lg lg:text-xl py-6">{banner.product_short_des}</p>
+                    <button className="btn btn-primary w-fit">Buy only ${banner.product_price}</button>
                 </div>
             </div>
         </div>
